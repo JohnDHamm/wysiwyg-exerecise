7a8d95e2de1bb94d60f7957b4845d11c30d9fa5f
John Hamm <johndhamm1@gmail.com>
2016-06-10 14:08:57 -0500

    finalized

--- eachElement.js
+++ eachElement.js
@@ -62,60 +62,56 @@ var textInput = document.getElementById('userInput');
 
 function changeText() {
 	// 8. When you click on one of the person elements, a dotted border should appear around it.
-	var selectedElIdNum = event.target.id.split("-")[1];
-	var selectedEl = document.getElementById(`personContainer-${selectedElIdNum}`);
+	var selectedEl = event.target;
 	selectedEl.classList.toggle("selectedBorder");
 
 // 9. When you click on one of the person elements, the text input should immediately gain focus so that you can start typing.
 	textInput.focus();
 	textInput.value = "";
 // 10. When there is a highlighted person element, and you begin typing in the input box, the person's biography should be immediately bound to what you are typing, letter by letter.
-	liveUpdate(selectedElIdNum, selectedEl);
+	liveUpdate(selectedEl);
 }
 
 
-function liveUpdate(selectedElIdNum, selectedEl) {
-	var selectedBio = document.getElementById(`bio-${selectedElIdNum}`)
+function liveUpdate(element) {
 	textInput.addEventListener('keyup', function(){
-			selectedBio.innerHTML = textInput.value;
+			element.innerHTML = textInput.value;
 	});
 // 11. When you press the enter/return key when typing in the input field, then the content of the input field should immediately be blank.
 	textInput.addEventListener('keypress', function (e) {
     var key = e.keyCode;
     if (key === 13) { // 13 is enter
-    		selectedEl.classList.toggle("selectedBorder");
-    		selectedBio = null;
+    		element.classList.toggle("selectedBorder");
     		textInput.value = "";
+    		element = "";
     };
 	});
 }
-
-
-
 
 // create the person cards from array
 
 for (i = 0; i < famousPeople.length; i++) {
 	var newDiv = `
-		<person id="personContainer-${i}" class="personContainer">
-			<header id="personHeader-${i}">
-				<h1 id="personName-${i}" class="personName">${famousPeople[i].name}</h1>
-				<h3 id="personTitle-${i}" class="personTitle">${famousPeople[i].title}</h3>
-			</header
+		<div id="personContainer-${i}" class="personContainer">
+			<h1 id="personName-${i}" class="personName">${famousPeople[i].name}</h1>
+			<h3 id="personTitle-${i}" class="personTitle">${famousPeople[i].title}</h3>
 
-			<section id="section-${i}">
-				<div id="photo-${i}" class="photoEl"><img src="${famousPeople[i].image}" class="photo"></div>
-				<p id="bio-${i}" class="bio">${famousPeople[i].bio}</p>
-			</section>
 
-			<footer id="footer-${i}">
-				<span class="notEditable">born:  </span><p id="yearBorn-${i}" class="year">${famousPeople[i].lifespan.birth}</p>
-				<span class="notEditable">died:  </span><p id="yearBorn-${i}" class="year">${famousPeople[i].lifespan.death}</p>
-			</footer>
-		</person>`
+			<div id="photo-${i}" class="photoEl"><img src="${famousPeople[i].image}" class="photo"></div>
+			<p id="bio-${i}" class="bio">${famousPeople[i].bio}</p>
+
+			<span class="notEditable">born:  </span><p id="yearBorn-${i}" class="year">${famousPeople[i].lifespan.birth}</p>
+			<span class="notEditable">died:  </span><p id="yearBorn-${i}" class="year">${famousPeople[i].lifespan.death}</p>
+			</div>`
 
 	outputEl.innerHTML += newDiv;
 };
+
+// 5. For every even numbered element, have a light yellow background.
+// 6. For every odd numbered element, have a light blue background.
+
+
+
 
 // add event listeners to person containers (encompassing div)
 for (i = 0; i < famousPeople.length; i++) {
